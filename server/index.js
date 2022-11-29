e4be964b4be5fb816226062e9aba67c21ff193e7
Vamsi <2143333@cognizant.com>
2022-11-29 20:08:02 +0530

    second commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+//require WebSocket module and store its ref in WebSocket const
+const WebSocket = require("ws");
+
+//from WebSocket.Server assign a port and store it in wss
+const wss= new WebSocket.Server({port:9001});
+
+wss.on('connection',ws=>{
+    console.log('new client got connected');
+ ws.on('message',data=>console.log(`received data ${data}`));
+ws.on('close',()=>{console.log('client disconnected')})
+
+    
+});
